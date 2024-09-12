8dab6fc37f743d9a73ca001ae202e302e3b4970e
Ilia <ilya11141890@gmail.com>
2024-09-12 19:12:17 +0300

    Added Dbcontext

---
+++ Beam.Server.Infrastructure/BeamDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Beam.Infrastructure;
+
+public class BeamDbContext : DbContext
+{
+    public BeamDbContext(DbContextOptions<BeamDbContext> options) : base(options)
+    {
+    }
+}
