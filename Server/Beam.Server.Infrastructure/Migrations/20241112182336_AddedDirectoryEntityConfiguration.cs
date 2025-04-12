4d99ef08f3f2fef56adcff5214092b291cc96216
Ilia <ilya11141890@gmail.com>
2025-04-12 14:32:01 +0300

    Added SignalR.

---
+++ Server/Beam.Server.Infrastructure/Migrations/20241112182336_AddedDirectoryEntityConfiguration.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beam.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDirectoryEntityConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostLikes_PostId",
+                table: "PostLikes",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentLikes_CommentId",
+                table: "CommentLikes",
+                column: "CommentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CommentLikes_Comments_CommentId",
+                table: "CommentLikes",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CommentLikes_Users_UserId",
+                table: "CommentLikes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostLikes_Posts_PostId",
+                table: "PostLikes",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostLikes_Users_UserId",
+                table: "PostLikes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CommentLikes_Comments_CommentId",
+                table: "CommentLikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CommentLikes_Users_UserId",
+                table: "CommentLikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostLikes_Posts_PostId",
+                table: "PostLikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostLikes_Users_UserId",
+                table: "PostLikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PostLikes_PostId",
+                table: "PostLikes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CommentLikes_CommentId",
+                table: "CommentLikes");
+        }
+    }
+}
---
+++ Server/Beam.Server.Infrastructure/Migrations/20250219184103_AddedFieldUserIsOnline.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beam.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFieldUserIsOnline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnline",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnline",
+                table: "Users");
+        }
+    }
+}
